c973cfd6bdb44e03c69aa7d070638d7545e48d97
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-02-25 13:22:15 +0530

    find min and max element in atmost 3*[n/2] comparisons

--- src/main.c
+++ src/main.c
@@ -1,18 +1,13 @@
 #include "countsort.h"
+#include "min_max.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main(int argc, char *argv[]) {
-  int A[] = {2, 5, 3, 0, 2, 3, 0, 3};
-  int k = 5;
+  int A[] = {2, 5, 3, 10, 2, -3, 0, 3};
   int n = sizeof(A) / sizeof(int);
-  for (int i = 0; i < n; i++)
-    printf("%d  ", A[i]);
-  printf("\n");
-  count_sort(A, n, k);
-  for (int i = 0; i < n; i++)
-    printf("%d  ", A[i]);
-  printf("\n");
+  int min, max;
+  min_and_max(A, n, &min, &max);
+  printf("%d %d\n", min, max);
   return 0;
 }
