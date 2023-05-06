fe2bfe819f2207e3a88fcfcefb75763670c1749e
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-05-06 17:34:39 +0530

    added DP algo for activity selection problem

--- activity_selection.c
+++ activity_selection.c
@@ -33,24 +33,65 @@ void greedy_activity_selector(activity *list, activity *selected, int *counter) 
   }
 }
 
+void print_dp_activities(int *S, int i, int j, int n) {
+  if (i < j) {
+    if (S[i * n + j] > 0) {
+      print_dp_activities(S, i, S[i * n + j], n);
+      printf("%d ", S[i * n + j]);
+      print_dp_activities(S, S[i * n + j], j, n);
+    }
+  }
+}
+
+void dp_activity_selector(activity *list, int n, int *C, int *S) {
+  for (int i = 0; i < n * n; i++) {
+    C[i] = 0;
+    S[i] = 0;
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      // find any activity k that starts after i finishes and finish before j starts.
+      for (int k = i + 1; k < j; k++) {
+        if (list[k].startTime > list[i].finishTime && list[k].finishTime < list[j].startTime) {
+          if (C[i * n + k] + C[k * n + j] + 1 > C[i * n + j]) {
+            C[i * n + j] = C[i * n + k] + C[k * n + j] + 1;
+            S[i * n + j] = k;
+          }
+        }
+      }
+    }
+  }
+  printf("DP Algo: ");
+  print_dp_activities(S, 0, n-1, n);
+}
+
 int main(int argc, char *argv[]) {
-  activity list[N] = {{0, 0, 0},
-                      {1, 1, 4},
-                      {2, 3, 5},
-                      {3, 0, 6},
-                      {4, 5, 7},
-                      {5, 3, 9},
-                      {6, 5, 9},
-                      {7, 6, 10},
-                      {8, 8, 11},
-                      {9, 8, 12},
-                      {10, 2, 14},
-                      {11, 12, 16}};
+  activity list[N + 1] = {{0, 0, 0},
+                          {1, 1, 4},
+                          {2, 3, 5},
+                          {3, 0, 6},
+                          {4, 5, 7},
+                          {5, 3, 9},
+                          {6, 5, 9},
+                          {7, 6, 10},
+                          {8, 8, 11},
+                          {9, 8, 12},
+                          {10, 2, 14},
+                          {11, 12, 16},
+                          // dummy activity only for dp
+                          {12, 20, 30}};
+  int C[N + 1][N + 1], S[N + 1][N + 1];
+  dp_activity_selector(list, N + 1, (int *)C, (int *)S);
+  printf("\n");
+
   int counter = 0;
   activity selected[N];
   greedy_activity_selector(list, selected, &counter);
+  printf("Greedy Algo: ");
   for (int i = 0; i < counter; i++)
     printf("%d ", selected[i].id);
   printf("\n");
+
   return 0;
 }
