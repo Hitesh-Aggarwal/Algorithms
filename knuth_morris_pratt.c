15597a981caff2f0e650eb8ffaa8d21f60eb51aa
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-05-19 11:43:02 +0530

    added KMP algo

---
+++ knuth_morris_pratt.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *compute_prefix_function(char *P) {
+  int m = strlen(P);
+  int *pi = malloc(sizeof(int) * m);
+  pi[0] = 0;
+  int k = 0;
+  for (int q = 2; q <= m; q++) {
+    while (k > 0 && P[k] != P[q - 1])
+      k = pi[k];
+    if (P[k] == P[q - 1]) k = k + 1;
+    pi[q - 1] = k;
+  }
+  return pi;
+}
+
+void KMP_matcher(char *T, char *P) {
+  int n = strlen(T);
+  int m = strlen(P);
+  int *pi = compute_prefix_function(P);
+  int q = 0;
+  for (int i = 0; i < n; i++) {
+    while (q > 0 && P[q] != T[i])
+      q = pi[q - 1];
+    if (P[q] == T[i]) q = q + 1;
+    if (q == m) {
+      printf("Pattern occurs with shift %d\n", i - m + 1);
+      q = pi[q - 1];
+    }
+  }
+  free(pi);
+}
+
+int main(int argc, char *argv[]) {
+  char *T = "bacbabababacaab";
+  char *P = "ababaca";
+  KMP_matcher(T, P);
+  printf("\n");
+  return 0;
+}
