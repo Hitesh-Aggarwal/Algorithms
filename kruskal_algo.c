956fb35e0db137cb764cc5d17be003596decf9f0
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-06-06 16:23:44 +0530

    prettily print the minimum spanning tree formed by kruskal's algorithm

--- kruskal_algo.c
+++ kruskal_algo.c
@@ -36,13 +36,39 @@ void link(Node *x, Node *y) {
 
 #define Union(x, y) link(find_set(x), find_set(y))
 
+void pretty_DFS_Visit(int graph[N][N], int u, char color[N], int indent) {
+  color[u] = 'G';
+  for (int i = 0; i < indent; i++)
+    printf("    |");
+  printf("----");
+  printf("%d\n", u);
+  for (int i = 0; i < N; i++) {
+    if (graph[u][i] == 1 && color[i] == 'W') { pretty_DFS_Visit(graph, i, color, indent + 1); }
+  }
+  color[u] = 'B';
+}
+
+void pretty_dfs(int graph[N][N]) {
+  char color[N];
+  for (int i = 0; i < N; i++)
+    color[i] = 'W';
+  for (int i = 0; i < N; i++) {
+    if (color[i] == 'W') pretty_DFS_Visit(graph, i, color, 0);
+  }
+}
+
 void kruskal(int graph[N][N]) {
+  int final_graph[N][N];
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
+      final_graph[i][j] = 0;
+
   Node *node_list[N];
   for (int i = 0; i < N; i++) {
     node_list[i] = create_node(i);
     make_set(node_list[i]);
   }
-  int u, v;
+  int u = 0, v = 1;
   int min;
   int edge_count = 0;
   while (edge_count < N - 1) {
@@ -58,8 +84,14 @@ void kruskal(int graph[N][N]) {
     }
     Union(node_list[u], node_list[v]);
     edge_count = edge_count + 1;
-    printf("Edge chosen: %d --- %d\n", u, v);
+    final_graph[u][v] = 1;
+    final_graph[v][u] = 1;
+    printf("Edge Selected: %d --- %d\n", u, v);
   }
+  printf("\nFinal Tree:\n");
+  pretty_dfs(final_graph);
+  for(int i=0; i<N; i++)
+    free(node_list[i]);
 }
 
 int main(int argc, char *argv[]) {
