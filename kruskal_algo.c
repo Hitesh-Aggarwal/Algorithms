700dcd36a1454f90c05d3f3fe98df925c612cb84
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-06-08 15:46:02 +0530

    kruskal's algo used disjoint set library

--- kruskal_algo.c
+++ kruskal_algo.c
@@ -1,3 +1,4 @@
+#include "disjoint_sets.h"
 #include "graph.h"
 #include <limits.h>
 #include <stdio.h>
@@ -5,50 +6,15 @@
 
 #define N 9
 
-typedef struct Node {
-  int key;
-  int rank;
-  struct Node *parent;
-} Node;
-
-Node *create_node(int key) {
-  Node *x = malloc(sizeof(Node));
-  x->key = key;
-  x->parent = NULL;
-  return x;
-}
-
-void make_set(Node *x) {
-  x->parent = x;
-  x->rank = 0;
-}
-
-Node *find_set(Node *x) {
-  if (x != x->parent) x->parent = find_set(x->parent);
-  return x->parent;
-}
-
-void link(Node *x, Node *y) {
-  if (x->rank > y->rank)
-    y->parent = x;
-  else { x->parent = y; }
-  if (x->rank == y->rank) y->rank = y->rank + 1;
-}
-
-#define Union(x, y) link(find_set(x), find_set(y))
-
 void kruskal(int graph[N][N]) {
   vertex final_graph[N];
   for (int i = 0; i < N; i++) {
     final_graph[i].index = i;
     final_graph[i].next = NULL;
   }
-
-  Node *node_list[N];
-  for (int i = 0; i < N; i++) {
-    node_list[i] = create_node(i);
-    make_set(node_list[i]);
-  }
+  initialize_sets(N);
+  for (int i = 0; i < N; i++)
+    make_set(i);
   int u = 0, v = 1;
   int min;
   int edge_count = 0;
@@ -56,14 +22,14 @@ void kruskal(int graph[N][N]) {
     min = INT_MAX;
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
-        if (find_set(node_list[i]) != find_set(node_list[j]) && graph[i][j] < min) {
+        if (find_set(i) != find_set(j) && graph[i][j] < min) {
           min = graph[i][j];
           u = i;
           v = j;
         }
       }
     }
-    Union(node_list[u], node_list[v]);
+    Union(u, v);
     edge_count = edge_count + 1;
     insert_edge(final_graph, u, v);
     insert_edge(final_graph, v, u);
@@ -71,13 +37,11 @@ void kruskal(int graph[N][N]) {
   }
   printf("\nFinal Tree:\n");
   pretty_print_dfs(final_graph, N);
-  for (int i = 0; i < N; i++)
-    free(node_list[i]);
-  free_graph(final_graph,N);
+  free_graph(final_graph, N);
+  destroy_sets();
 }
 
 int main(int argc, char *argv[]) {
-
   int graph[N][N] = {{0, 4, INT_MAX, INT_MAX, INT_MAX, INT_MAX, INT_MAX, 8, INT_MAX},
                      {4, 0, 8, INT_MAX, INT_MAX, INT_MAX, INT_MAX, 11, INT_MAX},
                      {INT_MAX, 8, 0, 7, INT_MAX, 4, INT_MAX, INT_MAX, 2},
